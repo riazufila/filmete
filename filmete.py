c6cef30f8b9380cb96244a27dc5a8590f40157aa
riazufila <46749569+riazufila@users.noreply.github.com>
2021-02-25 14:39:19 +0800

    Changed ready message

--- filmete.py
+++ filmete.py
@@ -14,7 +14,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f"{client.user} has connected to Discord!")
+    print(f"{client.user} is online!")
 
 
 client.run(TOKEN)
