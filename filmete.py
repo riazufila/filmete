115d3c4ad0f3734d3f43c2586ccf774031f9c10e
riazufila <46749569+riazufila@users.noreply.github.com>
2021-02-25 13:44:50 +0800

    Added base

---
+++ filmete.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+import discord
+from dotenv import load_dotenv
+
+# Load env file
+load_dotenv()
+
+# Assign variables
+token = os.getenv("DISCORD_TOKEN")
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f"{client.user} has connected to Discord!")
+
+
+client.run(token)
